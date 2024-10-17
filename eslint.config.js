8f1514c873bd052082ca7b2aef3dff6f854afd47
jlabrador3108 <jorgelabrador3108@gmail.com>
2024-10-17 01:01:57 -0500

    update to build

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,10 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
-      "eslint-disable-next-line @typescript-eslint/no-explicit-any": off,
+      "no-unused-vars": ["warn"],
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+
     },
   }
 );
